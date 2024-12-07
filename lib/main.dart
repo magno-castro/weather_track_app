5a1b2c3fdc3c85776d443e281ca36bccd0c61701
Magno Castro Moraes <magno.castro@niduu.com>
2024-12-07 09:50:20 -0300

    chore: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherTrackApp());
+}
+
+class WeatherTrackApp extends StatefulWidget {
+  const WeatherTrackApp({super.key});
+
+  @override
+  State<WeatherTrackApp> createState() => _WeatherTrackAppState();
+}
+
+class _WeatherTrackAppState extends State<WeatherTrackApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
